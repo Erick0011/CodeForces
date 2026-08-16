1d79634ea0bc31dd83122a3205e02924110340d1
Eric Vaz Baptista <106451611+Erick0011@users.noreply.github.com>
2026-08-16 14:44:38 +0100

    Add Perfect Root [A] from Codeforces

---
+++ 2185/A - Perfect Root/solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+ 
+ 
+    int n, t;
+ 
+    cin >> t;
+ 
+    while(t--) {
+        cin >> n;
+        for (int i = 0; i < n; i++) {
+            cout << n + i << "
+";
+        }
+    }
+    return 0;
+ 
+}
