69d489494302430af4f93aa25d33ef152d549397
Eric Vaz Baptista <106451611+Erick0011@users.noreply.github.com>
2026-08-16 15:30:24 +0100

    Add Ideal Generator [A] from Codeforces

---
+++ 2093/A - Ideal Generator/solution.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a;
+        cin >> a;
+ 
+        cout << (a % 2 == 0 ? "NO
+" : "YES
+");
+    }
+}
