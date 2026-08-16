871f67b19a58492212b7ef8f467ee523623d0f75
Eric Vaz Baptista <106451611+Erick0011@users.noreply.github.com>
2026-08-16 14:44:01 +0100

    Add Sum [A] from Codeforces

---
+++ 1742/A - Sum/solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+ 
+        if (a == b + c || b == a + c || c == b + a)
+        {
+            cout << "YES" << "
+";
+        }
+        else
+        {
+            cout << "NO" << "
+";
+        }
+    }
+}
