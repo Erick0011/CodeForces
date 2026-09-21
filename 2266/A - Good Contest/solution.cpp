84d76cda850fe913c82f6247c22ab49cefce1ac0
Eric Vaz Baptista <106451611+Erick0011@users.noreply.github.com>
2026-09-21 15:45:37 +0100

    Add Good Contest [A] from Codeforces

---
+++ 2266/A - Good Contest/solution.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+ 
+#define linhasDeCaos ios_base::sync_with_stdio(false), cin.tie(NULL);
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define F first
+#define S second
+#define pb push_back
+ 
+const ll MOD = 1000000007LL;
+const ll INF = 4e18;
+const int INF_INT = 1e9;
+const double EPS = 1e-9;
+ 
+int main()
+{
+    linhasDeCaos;
+    int t;
+    cin >> t;
+ 
+    while (t--)
+    {
+        int n, a;
+        int ans = INF_INT;
+        cin >> n;
+ 
+        for (int i = 0; i < 3; i++)
+        {
+            cin >> a;
+            ans = min(a, ans);
+        }
+        cout << n - ans << "
+";
+    }
+ 
+    return 0;
+}
