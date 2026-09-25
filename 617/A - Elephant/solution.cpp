b8b04d5ecff6a3d0a5610f358cbfdf1ee0aa7484
Eric Vaz Baptista <106451611+Erick0011@users.noreply.github.com>
2026-09-25 23:09:24 +0100

    Add Elephant [A] from Codeforces

---
+++ 617/A - Elephant/solution.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+ 
+#define linhasDeCaos ios_base::sync_with_stdio(false), cin.tie(NULL);
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define F first
+#define S second
+#define pb push_back
+ 
+const ll MOD = 1000000007LL;
+const ll INF = 4e18;
+const int INF_INT = 1e9;
+const double EPS = 1e-9;
+ 
+int main()
+{
+    linhasDeCaos;
+    int n, cont = 0;
+    cin >> n;
+    while (n > 0)
+    {
+        if (n - 5 >= 0)
+            n -= 5;
+        else if (n - 4 >= 0)
+            n -= 4;
+        else if (n - 3 >= 0)
+            n -= 3;
+        else if (n - 2 >= 0)
+            n -= 2;
+        else
+            n--;
+        cont++;
+    }
+    cout << cont;
+}
+ 
