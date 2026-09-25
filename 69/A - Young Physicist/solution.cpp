f1c4d4406ced42c35a63b5873f4c0814e2eaa582
Eric Vaz Baptista <106451611+Erick0011@users.noreply.github.com>
2026-09-25 23:59:36 +0100

    Add Young Physicist [A] from Codeforces

---
+++ 69/A - Young Physicist/solution.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+ 
+#define linhasDeCaos ios_base::sync_with_stdio(false), cin.tie(NULL);
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define F first
+#define S second
+#define pb push_back
+ 
+const ll MOD = 1000000007LL;
+const ll INF = 4e18;
+const int INF_INT = 1e9;
+const double EPS = 1e-9;
+ 
+int main()
+{
+    linhasDeCaos;
+ 
+    int n;
+    int x, y, z;
+    int sumX = 0, sumY = 0, sumZ = 0;
+    cin >> n;
+    while (n--)
+    {
+        cin >> x >> y >> z;
+        sumX += x;
+        sumY += y;
+        sumZ += z;
+    }
+    cout << (sumX == 0 && sumY == 0 && sumZ == 0 ? "YES
+" : "NO
+");
+}
